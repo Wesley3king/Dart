4e9b3aa5062ac3b2157a876d1b0a977172a7a154
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-07-24 21:46:16 -0300

    aula de variaveis no dart.

--- aulas/introdução/index.dart
+++ aulas/introdução/index.dart
@@ -1,3 +1,8 @@
 void main() {
-  print("hello world");
+  //var nomeCompleto = "king of shadows";
+  //String nomeCompleto = "king of shadows";
+  //Object nomeCompleto = "king of shadows";
+  dynamic nomeCompleto = "king of shadows";
+  nomeCompleto = 1;
+  print(nomeCompleto);
 }
