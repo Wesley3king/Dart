0a8bb5fd17fe42146a0579cb1d2678564fd84413
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-07-28 15:35:42 -0300

    aulas sobre operadores.

---
+++ aulas/operators/aritmetics.dart
@@ -0,0 +1,18 @@
+void main(List<String> args) {
+  print(5 + 6);
+  print(5 - 6);
+  print(5 * 6);
+  print(5 / 6);
+  print(15 % 4);
+
+  // operador de agrupamento
+  print(4 ~/ 2);
+
+  int num1 = 4;
+
+  print(num1++);
+  print(num1--);
+  print(num1);
+}
+
+//num soma(num n1, num n2) => n1 + n2;
---
+++ aulas/operators/igualdade.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  print(2 == 2); // igual
+  print(2 != 2); // diferente
+  print(2 > 3); // maior que
+  print(2 < 2); // menor que
+  print(2 >= 2); // maior ou igual a
+  print(2 <= 2); // menor ou igual a
+
+}
