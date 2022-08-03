08b6cfaad5a0e570411a866beb69232748e3ee87
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-08-03 16:17:55 -0300

    Create comentarios.dart
    aula de comentarios

---
+++ aulas/aulas_sem_descrição/comentarios.dart
@@ -0,0 +1,20 @@
+void main(List<String> args) {
+  print("hello world");
+
+  // comentario de linha unica
+  /*
+    comentario de varias linhas
+  */
+
+  // TODO: lanca uma mensagem na aba de problemas do terminal que sinaliza a necessidade de uma refatoração/inplementação
+}
+
+/// descreve o que uma função faz abaixo do status de uma função/classe [documentação] pedaco de código: `final String valor = '';`
+
+void metodo() {}
+
+/**
+ * este é uma documentação de varias linhas
+ * `void main() {}`
+ */
+void metodo2() {}
