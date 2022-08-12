a98063ec3735ea3118db7bc5d4cbaf4a4ec5ba08
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-08-11 21:47:55 -0300

    aulas de dart asincrono.
    introdução,
    Future,
    delayed,
    http.get,
    jsonDecode,
    fromJson,
    ToJson.

--- introduao_async/bin/introduao_async.dart
+++ introduao_async/bin/introduao_async.dart
@@ -1,12 +1,53 @@
-import 'package:test/scaffolding.dart';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 
 void main(List<String> arguments) {
-  Future.delayed(
-    Duration(seconds: 3),
-    () {
-      return 'hello world';
-    },
-  ).then((value) => print(value));
+  print('--------- iniciou ------');
+  fetch();
+}
 
-  print('niciando...');
+Future<void> fetch() async {
+  // https:// /todos/1
+  var url = Uri.https('jsonplaceholder.typicode.com', '/todos/1');
+
+  var response = await http.get(url);
+
+  print(response.body is Map);
+
+  var json = jsonDecode(response.body);
+
+  var todo = Todo.fromJson(json);
+
+  print(todo.title);
+}
+
+class Todo {
+  final String title;
+  final int userId;
+  final int id;
+  final bool completed;
+
+  Todo(
+      {required this.title,
+      required this.userId,
+      required this.id,
+      required this.completed});
+
+  factory Todo.fromJson(Map<String, dynamic> json) {
+    return Todo(
+        title: json['title'],
+        userId: json['userId'],
+        id: json['id'],
+        completed: json['completed']);
+  }
+
+  Map toJson() {
+    return {
+      "id": id,
+      "userId": userId,
+      "title": title,
+      "completed": completed,
+    };
+  }
 }
