0d0c6b104cc2d4dc90f6b1b9cce5c391c447f73a
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-10-07 16:21:23 -0300

    Update verificar_keys.dart

--- laboratory/verificar_keys.dart
+++ laboratory/verificar_keys.dart
@@ -1,9 +1,14 @@
 void main(List<String> args) {
   Map<String, dynamic> data = {"type": "king"};
+  Metodos metodo = Metodos();
 
   if (data.containsKey("type")) {
     print("tem este método!");
   } else {
     print("não tem este método");
   }
+}
+
+class Metodos {
+  final String type = "king";
 }
